884431c040fff2de62d4b88e600bbf69ae7bcb03
Erwin Bonsma <eriban@users.sourceforge.net>
2019-10-19 18:08:42 +0200

    Added part initialisation

--- StickPuzzleSolver.py
+++ StickPuzzleSolver.py
@@ -39,9 +39,49 @@ class Position:
   def __lt__(self, other):
     return ((self.orientation, self.location) < (other.orientation, other.location))
 
+# Table that defines how the rod and its sticks extend for the various orientations
+partRotations = {
+  Orientation.X: {
+    'rodDir': (1, 0, 0),
+    'stickDirs': [(0, 1, 0), (0, 0, 1)]
+  },
+  Orientation.Y: {
+    'rodDir': (0, 1, 0),
+    'stickDirs': [(0, 0, -1), (-1, 0, 0)]
+  },
+  Orientation.Z: {
+    'rodDir': (0, 0, -1),
+    'stickDirs': [(1, 0, 0), (0, -1, 0)]
+  }
+}
+
+def generateLocations(loc, dir, length):
+  return [(loc[0] + dir[0] * i, loc[1] + dir[1] * i, loc[2] + dir[2] * i) for i in range(length)]
+
 class Part:
-  def __init__(self, shapeNum):
-    self.shapeNum = shapeNum
+  def __init__(self, shape):
+    self.shape = shape
+
+    self.rodLoc = {}
+    self.stickLoc = {}
+
+    for rodOrientation in Orientation:
+      self.rodLoc[rodOrientation] = []
+      self.stickLoc[rodOrientation] = []
+
+      pr = partRotations[rodOrientation]
+      rodDir = pr['rodDir']
+
+      if len(shape) > 1:
+        self.rodLoc[rodOrientation].append(generateLocations((0, 0, 0), rodDir, len(shape)))
+
+      for i in range(len(shape)):
+        pos, neg = shape[i]
+
+        if pos != neg:
+          stickDir = pr['stickDirs'][i % 2]
+          refLoc = [rodDir[d] * i - stickDir[d] * neg for d in range(3)]
+          self.stickLoc[rodOrientation].append(generateLocations(refLoc, stickDir, pos + neg + 1))
 
 class Grid:
   def addPart(self, part, position, lowestConnectedPart = -1):
@@ -56,15 +96,22 @@ class Grid:
 
     # TODO
 
-  def getPositions(part, fromPart):
+  def getPositions(self, part, fromPart):
     # TODO
 
     return []
     
 
 def makeParts(numbers, shapes):
-  # TODO
-  return [Part(0)]
+  parts = []
+  for shapeNum in range(len(numbers)):
+    for p in range(numbers[shapeNum]):
+      part = Part(shapes[shapeNum])
+      part.index = len(parts)
+      part.shapeNum = shapeNum
+      parts.append(part)
+
+  return parts
 
 def solve(partNumber):
   if partNumber == len(parts):
@@ -73,17 +120,17 @@ def solve(partNumber):
 
   part = parts[partNumber]
 
-  sameShape = part[partNumber].shape == part[partNumber - 1].shape
+  sameShape = parts[partNumber].shape == parts[partNumber - 1].shape
   if sameShape:
-    startAtPartNumber = part[partNumber - 1].lowestConnectedPart
+    startAtPartNumber = parts[partNumber - 1].lowestConnectedPart
   else:
     startAtPartNumber = 0
 
   for fromPartNumber in range(startAtPartNumber, partNumber):
-    positions = grid.getPositions(part, part[fromPartNumber])
+    positions = grid.getPositions(part, parts[fromPartNumber])
 
     for pos in positions:
-      if not sameShape or pos > part[partNumber - 1].pos:
+      if not sameShape or pos > parts[partNumber - 1].pos:
         grid.addPart(part, pos, fromPartNumber)
         solve(partNumber + 1)
         grid.removePart(part, pos)
